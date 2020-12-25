9df1bdca318e5c7e9bea8b9fdc29e04ff3ef90ea
EvSevos <esev.zhe@gmail.com>
2020-12-26 00:52:22 +0200

    add  section "subscribe"  (html,css)+ "read more" for section "bouncy"

--- js/scripts.js
+++ js/scripts.js
@@ -152,6 +152,21 @@ function myFunction() {
         moreText.style.display = "inline";
     }
 }
+function myFunction1() {
+    var dots1 = document.getElementById("dots1");
+    var moreText1 = document.getElementById("more1");
+    var btnText1 = document.getElementById("myBtn1");
+
+    if (dots1.style.display === "none") {
+        dots1.style.display = "inline";
+        btnText1.innerHTML = "Read more";
+        moreText1.style.display = "none";
+    } else {
+        dots1.style.display = "none";
+        btnText1.innerHTML = "Read less";
+        moreText1.style.display = "inline";
+    }
+}
 function myFunction2() {
     var dots2 = document.getElementById("dots2");
     var moreText2 = document.getElementById("more2");
