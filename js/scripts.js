16ba776dc6ec95db454fcc8529437b7c0e85a089
EvSevos <esev.zhe@gmail.com>
2020-12-28 14:20:33 +0200

    chenge sector "details":add tabs and change header (nav)

--- js/scripts.js
+++ js/scripts.js
@@ -208,3 +208,20 @@ $(document).ready(function(){
         $('body,html').animate({scrollTop: top}, 1500);
     });
 });
+/*-------------tabs---------------*/
+function openIdea(event, ideaName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(ideaName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+// Получите элемент с id="defaultOpen" и нажмите на него
+document.getElementById("defaultOpen").click();
